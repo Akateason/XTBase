894289fba3288a5c7ebc9bbeb94805b5f34a49e0
谢天宸 <xietianchen@shimo.im>
2018-12-24 16:11:02 +0800

    dev update nslog define

--- XTBase/XTBase/header/XTlibConst.h
+++ XTBase/XTBase/header/XTlibConst.h
@@ -11,18 +11,21 @@
 #ifndef XTlibConst_h
 #define XTlibConst_h
 
-#define NSLog(format, ...)                                                                                                                    \
-    if ([XTlibConfig sharedInstance].isDebug) {                                                                                               \
-        do {                                                                                                                                  \
+#define NSLog(format, ...) \
+ do { \
+    if ([XTlibConfig sharedInstance].isDebug) {  \
             fprintf(stderr, "\n<---🏀🏀🏀🏀🏀\n");                                                                                                 \
-            fprintf(stderr, "<%s : %d> %s\n", [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, __func__); \
+            fprintf(stderr, "<%s : %d> %s\n",                                           \
+            [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],  \
+            __LINE__, __func__);                                                        \
             (NSLog)((format), ##__VA_ARGS__);                                                                                                 \
             fprintf(stderr, "🏀🏀🏀🏀🏀--->\n\n");                                                                                                 \
-        } while (0);                                                                                                                          \
-    }                                                                                                                                         \
-    else {                                                                                                                                    \
-        (NSLog)((format), ##__VA_ARGS__);                                                                                                     \
-    }
+    } \
+    else { \
+        (NSLog)((format), ##__VA_ARGS__); \
+    } \
+} while(0); \
+
 
 
 #endif /* XTlibConst_h */
