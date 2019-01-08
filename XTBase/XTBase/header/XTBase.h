92f2e6bbf824174eef59b01b51f247f0052b78f7
谢天宸 <xietianchen@shimo.im>
2019-01-08 15:59:42 +0800

    release

--- XTBase/XTBase/header/XTBase.h
+++ XTBase/XTBase/header/XTBase.h
@@ -14,6 +14,7 @@
 #import <YYModel/YYModel.h>
 #import <SVProgressHUD/SVProgressHUD.h>
 #import <ReactiveObjC/ReactiveObjC.h>
+#import <SDWebImage/UIImageView+WebCache.h>
 
 // header
 #import "ScreenHeader.h"
