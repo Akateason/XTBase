48a50b0ece99b649a1c85ff0a1107e928e32c607
谢天宸 <xietianchen@shimo.im>
2019-08-19 11:20:46 +0800

    add

--- XTBase/XTBase/Utils/UIkit/Views/Root/RootCustomView.h
+++ XTBase/XTBase/Utils/UIkit/Views/Root/RootCustomView.h
@@ -7,6 +7,20 @@
 
 // use in IB / Storyboard . inherit
 // ！！所有用到 @IBInspectable 功能的都要继承于 RootCustomView！！
+// 在Podfile中加入这段。解决xib render failed问题。
+/*
+post_install do |installer|
+installer.pods_project.targets.each do |target|
+target.new_shell_script_build_phase.shell_script = "mkdir -p $PODS_CONFIGURATION_BUILD_DIR/#{target.name}"
+target.build_configurations.each do |config|
+config.build_settings['CONFIGURATION_BUILD_DIR'] = '$PODS_CONFIGURATION_BUILD_DIR'
+config.build_settings.delete('CODE_SIGNING_ALLOWED')
+config.build_settings.delete('CODE_SIGNING_REQUIRED')
+end
+end
+end
+**********************************************************/
+
 
 #import <UIKit/UIKit.h>
 
