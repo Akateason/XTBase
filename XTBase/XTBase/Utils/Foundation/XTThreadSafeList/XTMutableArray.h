98a738c5d0d0f2b5e6305186fc60749454365658
谢天宸 <xietianchen@shimo.im>
2020-02-21 14:00:10 +0800

    add

--- XTBase/XTBase/Utils/Foundation/XTThreadSafeList/XTMutableArray.h
+++ XTBase/XTBase/Utils/Foundation/XTThreadSafeList/XTMutableArray.h
@@ -1,10 +1,24 @@
-//
+//  线程安全的数组
 //  XTMutableArray.h
 //  XTBase
 //
 //  Created by teason23 on 2018/11/26.
 //  Copyright © 2018 teason23. All rights reserved.
-//
+/**
+XTMutableArray *ary = [[XTMutableArray alloc] init];
+NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+queue.maxConcurrentOperationCount = 10;
+for (int i = 0; i < 200; i++) {
+    NSNumber *number = [NSNumber numberWithInt:i];
+    [queue addOperationWithBlock:^{
+        [ary addObject:number];
+    }];
+}
+[queue waitUntilAllOperationsAreFinished];
+
+NSLog(@"%ld", (long)ary.count);
+NSLog(@"%@", ary);
+*/
 
 #import "XTThreadSafeObject.h"
 
